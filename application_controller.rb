9ab692c628a5a710695da52361e52c402bf036a4
Daniel McDavid <daniel@SylsMacBookPro.local>
2016-06-29 15:17:13 -0400

    v1

--- application_controller.rb
+++ application_controller.rb
@@ -1,10 +1,13 @@
 require 'bundler'
+require_relative 'models/model.rb'
 Bundler.require
 
 class MyApp < Sinatra::Base
 
   get '/' do
+    @link=selector
     erb :index
+
   end
 
 end
---
+++ models/model.rb
@@ -0,0 +1,13 @@
+
+
+def selector
+  @linksArray = ["http://www.lingscars.com/",
+     "http://www.111111111111111111111111111111111111111111111111111111111111.com/",
+      "http://www.omfgdogs.com/",
+      "http://www.constellation7.org/Constellation-Seven/Josiah/Index.htm",
+      "http://www.trypap.com/"]
+
+  @link = @linksArray.sample
+end
+
+selector
